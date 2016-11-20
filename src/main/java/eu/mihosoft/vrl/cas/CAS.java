24d75ba359b3144ead2d58e635f49f9d9c515d8e
Michael Hoffer <info@michaelhoffer.de>
2016-11-20 22:11:36 +0100

    initial prototype

---
+++ src/main/java/eu/mihosoft/vrl/cas/CAS.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.mihosoft.vrl.cas;
+
+import eu.mihosoft.vrl.annotation.ComponentInfo;
+import java.io.Serializable;
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.ExprEvaluator;
+
+/**
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+@ComponentInfo(name = "CAS", category = "VRL/Math/CAS")
+public class CAS implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private transient EvalEngine engine;
+    private transient ExprEvaluator util;
+
+    public String process(String input) {
+        return getUtil().evaluate(input).toString();
+    }
+
+    private ExprEvaluator getUtil() {
+        if (util == null) {
+            util = new ExprEvaluator(getEngine(), false, 100);
+        }
+
+        return util;
+    }
+
+    private EvalEngine getEngine() {
+        if (engine == null) {
+            engine = new EvalEngine(true);
+        }
+
+        return engine;
+    }
+
+}
---
+++ src/main/java/eu/mihosoft/vrl/cas/Derive.java
@@ -0,0 +1,19 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package eu.mihosoft.vrl.cas;
+//
+//import eu.mihosoft.vrl.annotation.ParamInfo;
+//
+///**
+// *
+// * @author Michael Hoffer <info@michaelhoffer.de>
+// */
+//public class Derive {
+//    public String derive(@ParamInfo(name="Expression", style="editor") String input, ) {
+//        CAS cas = new CAS();
+//        cas.process("D("+input+"")
+//    }
+//}
