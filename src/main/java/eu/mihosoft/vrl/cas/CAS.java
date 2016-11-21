de5ba19fbda700e0e9c1f8603d7b6276fd03b350
Michael Hoffer <info@michaelhoffer.de>
2016-11-21 12:10:54 +0100

    ensure that the CAS engine is used on one thread only per instance

--- src/main/java/eu/mihosoft/vrl/cas/CAS.java
+++ src/main/java/eu/mihosoft/vrl/cas/CAS.java
@@ -6,6 +6,7 @@ package eu.mihosoft.vrl.cas;
 
 import eu.mihosoft.vrl.annotation.ComponentInfo;
 import eu.mihosoft.vrl.annotation.ParamInfo;
+import eu.mihosoft.vrl.visual.VSwingUtil;
 import java.io.Serializable;
 import org.matheclipse.core.basic.Config;
 import org.matheclipse.core.eval.EvalEngine;
@@ -24,10 +25,13 @@ public class CAS implements Serializable {
     private transient ExprEvaluator util;
 
     public String process(@ParamInfo(name="Expression", style="code") String input) {
-        return getUtil().evaluate(input).toString();
+        String[] result = {""};
+        VSwingUtil.invokeAndWait(()->result[0]=getUtil().evaluate(input).toString());
+        return result[0];
     }
 
     private ExprEvaluator getUtil() {
+   
         if (util == null) {
             util = new ExprEvaluator(getEngine(), false, 100);
         }
